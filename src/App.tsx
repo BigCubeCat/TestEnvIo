795a084cd2d1741daceb03c75a6056139ed5d338
BigCubeCat <bigcubecat@yandex.ru>
2023-02-21 00:14:42 +0700

    AppBar skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import { useState } from 'react'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import AppBar from './components/Header/AppBar';
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: '#003790',
+    },
+    secondary: {
+      main: '#fc5055',
+    },
+  },
+});
+
 
 function App() {
-  return <div>hi
-  </div>
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="app">
+        <AppBar />
+      </div>
+    </ThemeProvider>
+  )
 }
 
 export default App
